5eb7d97f6797c2bb0ada08c10ca1a386d90570a8
Светлана <46722020+Sirenevaya@users.noreply.github.com>
2020-09-29 14:17:09 +0300

    Некоторые изменения по 1 части задания

--- js/main.js
+++ js/main.js
@@ -1,80 +1,99 @@
 "use strict";
 
-const USER_MESSAGES = [
-  `Всё отлично!`,
-  `В целом всё неплохо. Но не всё.`,
-  `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
-  `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
-  `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
-  `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
-];
-const USER_NAMES = [
-  `Артем`,
-  `Дмитрий`,
-  `Анна`,
-  `Татьяна`,
-  `Александр`,
-  `Полина`,
-];
-const NUMBER_COMMENTS = getRandomInt(1, 7);
-const NUMBER_PICTURE = 25;
+(function () {
 
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  const USER_MESSAGES = [
+    `Всё отлично!`,
+    `В целом всё неплохо. Но не всё.`,
+    `Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.`,
+    `Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.`,
+    `Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.`,
+    `Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!`
+  ];
+  const USER_NAMES = [
+    `Артем`,
+    `Дмитрий`,
+    `Анна`,
+    `Татьяна`,
+    `Александр`,
+    `Полина`,
+  ];
+  const NUMBER_PICTURE = 25;
 
-const getComments = function () {
-  const arrayComments = [];
-  for (let i = 0; i < NUMBER_COMMENTS; i++) {
-    arrayComments.push({
-      avatar: `img/avatar-` + getRandomInt(1, 6) + `.svg`,
-      message: USER_MESSAGES[Math.floor(Math.random() * USER_MESSAGES.length)],
-      name: USER_NAMES[Math.floor(Math.random() * USER_NAMES.length)]
-    });
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
-  return arrayComments;
-};
 
-const getRandomComments = function () {
-  const comments = [];
-  for (let i = 0; i < getRandomInt(0, 10); i++) {
-    comments.push(getComments());
+  const getUserComments = () => {
+    const arrayComments = [];
+    for (let i = 0; i < getRandomInt(1, 7); i++) {
+      arrayComments.push({
+        avatar: `img/avatar-${getRandomInt(1, 6)}.svg`,
+        message: USER_MESSAGES[Math.floor(Math.random() * USER_MESSAGES.length)],
+        name: USER_NAMES[Math.floor(Math.random() * USER_NAMES.length)]
+      });
+    }
+    return arrayComments;
+  };
+
+  const getArrayComments = () => {
+    const comments = [];
+    for (let i = 0; i < getRandomInt(0, 10); i++) {
+      comments.push(getUserComments());
+    }
+    return comments;
+  };
+
+  const getUserPictures = () => {
+    const arrayUserPictures = [];
+    for (let i = 1; i <= NUMBER_PICTURE; i++) {
+      arrayUserPictures.push({
+        url: `photos/${i}.jpg`,
+        description: `Описание фотографии`,
+        likes: getRandomInt(15, 200),
+        comments: getArrayComments()
+      });
+    }
+    return arrayUserPictures;
+  };
+
+  const sectionPictures = document.querySelector(`.pictures`);
+  const pictureTemplate = document.querySelector(`#picture`)
+    .content
+    .querySelector(`.picture`);
+
+  const outputPictures = (data) => {
+    const pictureElement = pictureTemplate.cloneNode(true);
+    pictureElement.querySelector(`.picture__likes`).textContent = data.likes;
+    pictureElement.querySelector(`.picture__comments`).textContent = data.comments.length;
+    pictureElement.querySelector(`.picture__img`).src = data.url;
+    return pictureElement;
+  };
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < getUserPictures().length; i++) {
+    fragment.appendChild(outputPictures(getUserPictures()[i]));
   }
-  return comments;
+  sectionPictures.appendChild(fragment);
+
+})();
+
+
+/* const bigPicture = document.querySelector(`.big-picture`);
+bigPicture.classList.remove(`hidden`);
+
+/* const bigPictureImg = bigPicture.querySelector(`.big-picture__img img`); */
+
+/* const getBigPicture = function (datapicture) {
+  bigPicture.querySelector(`.big-picture__img img`).src = datapicture.url;
+  bigPicture.querySelector(`.social__caption`).textContent = datapicture.description;
+  bigPicture.querySelector(`.likes-count`).textContent = datapicture.likes;
+  bigPicture.querySelector(`.comments-count`).textContent = datapicture.comments.length;
 };
 
-const getUserPhoto = function () {
-  const arrayPhoto = [];
-  for (let i = 1; i <= NUMBER_PICTURE; i++) {
-    arrayPhoto.push({
-      url: `photos/` + i + `.jpg`,
-      description: ``,
-      likes: getRandomInt(15, 200),
-      comments: getRandomComments()
-    });
-  }
-  return arrayPhoto;
-};
 
-const getPicture = getUserPhoto();
-
-const sectionPictures = document.querySelector(`.pictures`);
-const pictureTemplate = document.querySelector(`#picture`)
-  .content
-  .querySelector(`.picture`);
-
-const outputPicture = function (data) {
-  const pictureElement = pictureTemplate.cloneNode(true);
-
-  pictureElement.querySelector(`.picture__likes`).textContent = data.likes;
-  pictureElement.querySelector(`.picture__comments`).textContent = data.comments.length;
-  pictureElement.querySelector(`.picture__img`).src = data.url;
-
-  return pictureElement;
-};
-
-const fragment = document.createDocumentFragment();
 for (let i = 0; i < getPicture.length; i++) {
-  fragment.appendChild(outputPicture(getPicture[i]));
-}
-sectionPictures.appendChild(fragment);
+  bigPicture.innerHTML(getBigPicture(getPicture[i]));
+} * / */
+
+
